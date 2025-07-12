e1235fba3871543f919196e12cd7dfb678f74aa5
kyewu <18336768355@163.com>
2025-07-12 16:01:34 +0800

    show breadcrumb at top or mix mode

--- src/components/Layouts/Header.vue
+++ src/components/Layouts/Header.vue
@@ -7,7 +7,7 @@
       v-if="settings?.mode !== 'top'"
       v-show="showCollapse"
     ></Iconify>
-    <VBreadcrumb></VBreadcrumb>
+    <VBreadcrumb v-if="['top', 'mix'].includes(settings?.mode || '')"></VBreadcrumb>
     <div class="relative flex-grow overflow-x-hidden">
       <slot></slot>
     </div>
